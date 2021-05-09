613fd993dd7a6ff84becde2f9ac81fe23a295343
JagTheFriend <jagthefriend12@gmail.com>
2021-05-09 11:19:54 +0000

    Added the functions to a class called: "Task1"
    I've put the all the code used for task 1 under the class name: `Task1
    And I've made those functions static

--- Teacher's Work/Due_6_May_2021.dart
+++ Teacher's Work/Due_6_May_2021.dart
@@ -1,55 +1,57 @@
 import 'dart:io';
 
-int take_age(){
-    /**
-    * Returns the age of the Student
-    * Return Type: int
-    */
-  
-    int age;
-    // Allow the user to enter the age
-    print("\nPlease enter the age: ");
-    age = int.parse(stdin.readLineSync() as String);
+class Task1 {
+    static int take_age(){
+        /**
+        * Returns the age of the Student
+        * Return Type: int
+        */
+    
+        int age;
+        // Allow the user to enter the age
+        print("\nPlease enter the age: ");
+        age = int.parse(stdin.readLineSync() as String);
 
-    // && = And
-    if (age <= 16 && age >= 12){
-        return age;
+        // && = And
+        if (age <= 16 && age >= 12){
+            return age;
+        }
+        // invalid age was given
+        print("Please enter a valid age!");
+        take_age();
+
+        return 0;
     }
-    // invalid age was given
-    print("Please enter a valid age!");
-    take_age();
 
-    return 0;
-}
+    static String take_school_name(){
+        /**
+         * Asks for the name of the school the student is in
+         * Return Type: String
+         */
+        String school;
+        // Allow the user to enter the school name
+        print("\nPlease enter the name of the school: ");
+        school = stdin.readLineSync() as String;
 
-String take_school_name(){
-    /**
-     * Asks for the name of the school the student is in
-     * Return Type: String
-     */
-    String school;
-    // Allow the user to enter the school name
-    print("\nPlease enter the name of the school: ");
-    school = stdin.readLineSync() as String;
-
-    // || = Or
-    if (school == "mars" || school == "saturn"){
-        return school;
+        // || = Or
+        if (school == "mars" || school == "saturn"){
+            return school;
+        }
+        // invalid school name was given
+        print("Please enter a valid school name!");
+        take_school_name();
+        return "";
     }
-    // invalid school name was given
-    print("Please enter a valid school name!");
-    take_school_name();
-    return "";
-}
 
-double take_reaction_time(){
-    /**
-    * Returns the reaction time of the Student
-    * Return Type: Double
-    */
-    // Allow the user to enter the age
-    print("\nPlease enter the reaction time: ");
-    return double.parse(stdin.readLineSync() as String);
+    static double take_reaction_time(){
+        /**
+        * Returns the reaction time of the Student
+        * Return Type: Double
+        */
+        // Allow the user to enter the age
+        print("\nPlease enter the reaction time: ");
+        return double.parse(stdin.readLineSync() as String);
+    }
 }
 
 void main(){
@@ -60,9 +62,9 @@ void main(){
     var reaction_time = new List<double>.filled(5, 0, growable: false);  
 
     for (var i = 0; i < student_age.length; i++) {
-        student_age[i] = take_age(); // gets the age
-        school[i] = take_school_name(); // gets the school name
-        reaction_time[i] = take_reaction_time(); // gets the reaction time
+        student_age[i] = Task1.take_age(); // gets the age
+        school[i] = Task1.take_school_name(); // gets the school name
+        reaction_time[i] = Task1.take_reaction_time(); // gets the reaction time
     }
     print("Student age: $student_age");
     print("School Name: $school");
