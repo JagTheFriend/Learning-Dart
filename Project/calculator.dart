9ea12d7ce77e34c48773a5476cf44dfc831e3104
JagTheFriend <jagthefriend12@gmail.com>
2021-05-12 21:28:06 +0200

    Formatted File

--- Project/calculator.dart
+++ Project/calculator.dart
@@ -1,26 +1,26 @@
 import 'dart:io';
 
-double return_result(double number_1, double number_2, String operator_1){
+double return_result(double number_1, double number_2, String operator_1) {
   /**Performs the calculation and returns the result*/
   switch (operator_1) {
     case "+":
       return number_1 + number_2;
-    
-    case "*": 
+
+    case "*":
       return number_1 * number_2;
-    
-    case "-": 
+
+    case "-":
       return number_1 - number_2;
-    
-    case "/": 
+
+    case "/":
       return number_1 / number_2;
-    
+
     default:
       return 69;
   }
 }
 
-void get_output(double number_1, double number_2, String operator_1){
+void get_output(double number_1, double number_2, String operator_1) {
   /**This it the core function
    * It handles the flow return values of the different functions
    * such as checking whether the given operator (operator_1) is valid 
@@ -28,13 +28,13 @@ void get_output(double number_1, double number_2, String operator_1){
    * And showing the ouput on the console(or the screen/terminal)
    */
   String operator_ = get_operator(operator_1);
-  
-  if (operator_ != "Invalid"){
+
+  if (operator_ != "Invalid") {
     print("Output: $return_result(number_1, number_2, operator_1)");
   }
   print("Ivalid operator!!\n");
 }
-  
+
 String get_operator(String operator_1) {
   /**
    * Finds which operator was used
@@ -42,25 +42,25 @@ String get_operator(String operator_1) {
    */
   List<String> f = ["+", "-", "*", "/"];
   for (var i in f) {
-    if (i == operator_1){
+    if (i == operator_1) {
       return i;
     }
-  }  
+  }
   // invalid operator
   return "Invalid";
 }
 
 void main() {
-  while(true){
+  while (true) {
     // get numbers:
     print("Enter a number: ");
-    double number_1 = double.parse(stdin.readLineSync() as  String); 
-    
+    double number_1 = double.parse(stdin.readLineSync() as String);
+
     print("Enter another number:");
-    double number_2 = double.parse(stdin.readLineSync() as  String); 
+    double number_2 = double.parse(stdin.readLineSync() as String);
 
     print("Enter the operator:");
-    String operator_1 = stdin.readLineSync() as  String;
+    String operator_1 = stdin.readLineSync() as String;
     get_output(number_1, number_2, operator_1);
   }
 }
